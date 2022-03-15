20fb4c0824864815dde5d52f6fb84624ee22a934
wa_ab <waqas@itinsync.com>
2022-03-15 14:57:18 +0500

    New Configurations

--- src/main/java/com/example/webflux/WebfluxApplication.java
+++ src/main/java/com/example/webflux/WebfluxApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
@@ -29,6 +28,10 @@ public class WebfluxApplication {
                 .doOnConnected(conn ->
                         conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
                                 .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+        HttpClient httpClient1 = (HttpClient) HttpClient.create()
+                .baseUrl("http://localhost:8081")
+                .get();
+
         WebClient client = WebClient.builder()
                 .baseUrl("http://localhost:8080")
                 .defaultCookie("cookieKey", "cookieValue")
@@ -41,17 +44,17 @@ public class WebfluxApplication {
 //                .build();
         System.out.println("Hello From Spring Web Let's Say");
         System.out.println("Clients Data is\n" + client.get() + "\n" + httpClient);
-        try{
-            URL url=new URL("https://github.com/");
-            HttpURLConnection con=(HttpURLConnection) url.openConnection();
+        try {
+            URL url = new URL("https://github.com/");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            con.setRequestProperty("User-Agent","Java 1.1");
-            if (con.getResponseCode()==200){
+            con.setRequestProperty("User-Agent", "Java 1.1");
+            if (con.getResponseCode() == 200) {
                 System.out.println(con.getInputStream());
-            }else {
+            } else {
                 System.out.println("Error in Connection");
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
